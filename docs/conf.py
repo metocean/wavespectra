6f6ca246877e016b0c6f332952341804873280c5
Jorge Perez <33044656+jorgeperezg@users.noreply.github.com>
2020-08-27 13:07:09 +1200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,10 @@
 #
 import os
 import sys
-import wavespectra
 
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath("../wavespectra"))
+sys.path.insert(0, os.path.abspath("../"))
+import wavespectra
 
 # -- General configuration ------------------------------------------------
 
