fe2ade31e8c6e01c18840924180267a8ccfabd90
gael <gael.arnaud@metocean.co.nz>
2021-02-09 15:05:30 +1300

    changed dir into float64 to allow plots

--- wavespectra/output/ww3.py
+++ wavespectra/output/ww3.py
@@ -54,6 +54,7 @@ def to_ww3(self, filename, ncformat="NETCDF4", compress=False):
     other["frequency2"] = other[attrs.FREQNAME] + df
     # Direction in going-to convention
     other[attrs.DIRNAME] = (other[attrs.DIRNAME] + 180) % 360.
+    other['dir'] = other.dir.astype(np.float64)
     # station_name variable
     arr = np.array([[c for c in "{:06.0f}".format(s)] + [""] * 10 for s in other.site.values], dtype="|S1")
     other["station_name"] = xr.DataArray(
