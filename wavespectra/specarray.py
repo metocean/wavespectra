0da75462fcb1aef7a4075e2cdf77ebe55c465925
hrapizo <h.rapizo@metocean.co.nz>
2024-10-17 13:23:39 +1300

    modifications to some existing methods and adding some extra nonlinear markers

--- wavespectra/specarray.py
+++ wavespectra/specarray.py
@@ -873,7 +873,7 @@ class SpecArray(object):
         raise NotImplementedError(
             "Wave spreading at the peak wave frequency method not defined"
         )
-        
+
     def cross_sea_Li2016(self):
         """Crossed Sea parameters by Li et al.,2016
          Li, X. (2016). A new insight from space into swell propagation and crossing in the global oceans. Geophysical Research Letters, 43, 5202 - 5209.
@@ -916,43 +916,41 @@ class SpecArray(object):
             raise ValueError(
              "only works on partitionned dataset, you need to call dspart = ds.spec.partition(ds.wspd, ds.wdir, ds.dpt) before hand")
 
-    def r_crest_trough_correlation(self) : 
-        """Compute crest-trough correlation of the wave record from spectral density.
-         crest-trough correlation "r"" is the auto-correlation of the sea surface elevation at half the wave period. 
+    def r_crest_trough_corr(self) :
+        """ crest-trough correlation "r" of the wave record from spectral density.
+            r is the auto-correlation of the sea surface elevation at half the wave period.
 
         Reference:
-            Tayfun, M. Aziz, and Francesco Fedele. "Wave-Height Distributions and Nonlinear Effects."
-            Ocean Engineering, vol. 34, no. 11, Aug. 2007, pp. 1631–49. ScienceDirect,
-            doi:10.1016/j.oceaneng.2006.11.006.
-
-            Gemmrich, J., & Cicon, L. (2022). Generation mechanism and prediction of an observed extreme rogue wave. Scientific Reports, 12.
-
-            Adapted from code in https://github.com/dionhaefner/FOWD/blob/master/fowd/operators.py#L428
+            Gemmrich, J., & Cicon, L. (2022). Generation mechanism and prediction
+            of an observed extreme rogue wave. Scientific Reports, 12.
+            which is based on:
+            Tayfun, M. Aziz, and Francesco Fedele. Wave-Height Distributions and
+            Nonlinear Effects. Ocean Eng, vol. 34, no. 11, Aug. 2007, pp. 1631-49.
         """
-        # convert f -> omega and S_f -> S_omega
-        angular_frequencies = 2 * np.pi * self.freq# 2 * np.pi * frequencies
-        angular_density = self.oned().copy() / (2 * np.pi)# wave_spectral_density / (2 * np.pi) . Note : self._obj is xarray obj
+        # convert f -> omega and S(f) -> S(omega): Simon originally wrote with angular frequency, but in the
+        # end used m0 based on frequency to calculate "r", which I found inconsistent. This follow The Tayfun Fedele
+        # paper, but they base all their calculations on angular spectral density. Gemmrich and Cicon 2022 express
+        # r based on frequency and S(f), so I will follow that here as it is consistent with our m0.
 
-        # first moment in frequency domain -> no factor pi/2
-        t_bar = self.momf(0).sum(dim=attrs.DIRNAME) / self.momf(1).sum(dim=attrs.DIRNAME) #zeroth_moment / first_moment
+        # Calculate \tau and T
+        m0 = self.momf(0).sum(dim=attrs.DIRNAME)*self.dd
+        m1 = self.momf(1).sum(dim=attrs.DIRNAME)*self.dd
+        t_bar = m0 / m1
+        tau = t_bar/2
+        arg = 2 * np.pi * self.freq * tau
+        rho    = (self.oned() * np.cos(arg) * self.dfarr).sum(dim=attrs.FREQNAME)
+        lambd  = (self.oned() * np.sin(arg) * self.dfarr).sum(dim=attrs.FREQNAME)
 
-        arg = angular_frequencies * t_bar / 2.
-
-        c_rho    = np.trapz(angular_density * np.cos(arg), angular_frequencies) # c_rho = integrate(angular_density * np.cos(arg), angular_frequencies)
-        c_lambda = np.trapz(angular_density * np.sin(arg), angular_frequencies) # c_lambda = integrate(angular_density * np.sin(arg), angular_frequencies)
-
-        mom0 = self.momf(0).sum(dim=attrs.DIRNAME)*self.dd #same as self._twod(self.momf(0), dim=attrs.FREQNAME).spec.oned(skipna=False)
-
-        r_crest_trough_correlation = (1. /mom0) * np.sqrt(c_rho ** 2 + c_lambda ** 2) #1. / zeroth_moment * np.sqrt(c_rho ** 2 + c_lambda ** 2)        
-        r_crest_trough_correlation.attrs.update(
+        r_crest_trough_corr = (1. /m0) * np.sqrt(rho ** 2 + lambd ** 2)
+        r_crest_trough_corr.attrs.update(
             OrderedDict(
                 (
-                    ("standard_name", 'r_crest_trough_correlation'), # self.standard_name(self._my_name())
+                    ("standard_name", 'r_crest_trough_corr'), # self.standard_name(self._my_name())
                     ("units",'dimensionless'),             # self._units(self._my_name())
                 )
             )
         )
-        return r_crest_trough_correlation.rename('r_crest_trough_correlation') # or r_crest_trough_correlation.rename('r_crest_trough_correlation') ?
+        return r_crest_trough_corr.rename('r_crest_trough_corr') # or r_crest_trough_corr.rename('r_crest_trough_corr') ?
 
     def wavenumber(self,wave_period =10.0, water_depth=None):
         """Compute wave number for user-input wave period"""
@@ -963,18 +961,16 @@ class SpecArray(object):
             wavenumber = 2 * np.pi / (1.56 * wave_period ** 2)
         return wavenumber.rename('wavenumber')
 
-    def steepness(self):
-        """Compute characteristic steepness from peak wave number.
-         adapted from https://github.com/dionhaefner/FOWD/blob/master/fowd/operators.py#L374
-         
+    def steepness(self, tail=True):
+        """
         Reference
             Häfner, Dion, Johannes Gemmrich and Markus Jochum. “FOWD: A Free Ocean Wave Dataset 
             for Data Mining and Machine Learning.” Journal of Atmospheric and Oceanic Technology (2020)
             >> Equation (9)
         """
+        m0 = self.momf(0).sum(dim=attrs.DIRNAME)*self.dd
+        steepness = self.wavenumber(self.tp()) * np.sqrt(m0)
 
-        mom0 = self.momf(0).sum(dim=attrs.DIRNAME)*self.dd# self._twod(self.momf(0), dim=attrs.FREQNAME).spec.oned(skipna=False)
-        steepness = np.sqrt(2 * mom0) * self.wavenumber(self.tp())
         steepness.attrs.update(
             OrderedDict(
                 (
@@ -984,9 +980,6 @@ class SpecArray(object):
             )
         )
         return steepness.rename('steepness')
-        
-        # alternative equation could simply be 
-        # self.hs() / (2*np.pi/self.peak_wavenumber())
 
     def goda_peakedness(self):
         """Compute Goda's peakedness factor.
@@ -1000,12 +993,12 @@ class SpecArray(object):
             for Data Mining and Machine Learning.” Journal of Atmospheric and Oceanic Technology (2020)
             >> Equation (10) : "peakedness"
 
-            see also 
+            see also
             https://apps.ecmwf.int/codes/grib/param-db/?id=140254
             https://github.com/dionhaefner/FOWD/blob/7e4f2c3b1be6f55a0f399f6e48a39dc5507c9afe/fowd/operators.py#L386
         """
-        mom0 = self.momf(0).sum(dim=attrs.DIRNAME)*self.dd# self._twod(self.momf(0), dim=attrs.FREQNAME).spec.oned(skipna=False)
-        Qp = (2 / mom0 ** 2) * (self.freq * self.oned() ** 2 * self.dfarr).sum(dim=attrs.FREQNAME)
+        m0 = self.momf(0).sum(dim=attrs.DIRNAME)*self.dd# self._twod(self.momf(0), dim=attrs.FREQNAME).spec.oned(skipna=False)
+        Qp = (2 / m0 ** 2) * (self.freq * self.oned() ** 2 * self.dfarr).sum(dim=attrs.FREQNAME)
         Qp.attrs.update(
             OrderedDict(
                 (
@@ -1016,11 +1009,11 @@ class SpecArray(object):
         )
         return Qp.rename('goda_peakedness')
 
-    def BFI(self,water_depth = 1000.0): #bandwidth, steepness, peak_wavenumber):
+    def BFI(self): #bandwidth, steepness, peak_wavenumber):
         """Compute Benjamin-Feir index (BFI) from bandwidth and steepness estimates.
            Useful index for estimating the freak wave occurrence for a given unidirectional wave
 
-        
+
         References:
             Serio, Marina, et al. “On the Computation of the Benjamin-Feir Index.”
             Nuovo Cimento Della Societa Italiana Di Fisica C, vol. 28, Nov. 2005, pp. 893–903.
@@ -1034,17 +1027,12 @@ class SpecArray(object):
             Gemmrich, J., & Cicon, L. (2022). Generation mechanism and prediction of an observed extreme rogue wave. Scientific Reports, 12.
 
             Liu et al., 2022 Statistical properties of surface gravity waves and freak wave occurrence in crossing sea states,
-            PHYSICAL REVIEW FLUIDS 7, 074805 (2022) 
+            PHYSICAL REVIEW FLUIDS 7, 074805 (2022)
             Eqs (9)
 
-        """            
+        """
         # Simple version following Gemmrich, J., & Cicon, L. (2022). Eq. 4,5,6
-        mom0 = self.momf(0).sum(dim=attrs.DIRNAME)*self.dd# note mom0 is also equivalent to the variance of sea state 
-        wave_steepness_peak = np.sqrt(self.wavenumber(self.tp())**2 * mom0) # eqn (5)
-        wave_steepness_mean = np.sqrt(self.wavenumber(self.tm01())**2 * mom0) # eqn (5)
-        spectral_bandwidth = self.sw() # eqn (6) same as self.sw()
-        BFI = np.sqrt(2) * wave_steepness_peak/spectral_bandwidth # eqn(4)
-        # BFI = np.sqrt(2) * wave_steepness_mean/spectral_bandwidth # some authors suggest to use the mean steepness...
+        BFI = np.sqrt(2) * self.steepness()/self.sw() # eqn(4)
         BFI.attrs.update(
             OrderedDict(
                 (
@@ -1053,7 +1041,31 @@ class SpecArray(object):
                 )
             )
         )
-        return BFI.rename('BFI')
+        return BFI.rename("BFI")
+
+    def directionality_index(self):
+        R = 1/2 * ((D2R*self.dspr())**2)/((self.sw())**2)
+        return R.rename("directionality_index")
+
+    def BFI2D(self):
+        a2 = 7.1
+        # convert dspr to radians
+        R = self.directionality_index()
+        return (self.BFI()/np.sqrt(1 + a2 * R)).rename("BFI2D")
+
+    def kurtosis(self):
+        """Compute kurtosis according to Mori and Janssen JPO 2011
+        The value provided here is the fourth-order cumulant of the surface
+        elevation probability distribution k40, which is equal to
+        kurtosis-3 (k40 = u4 - 3)
+
+        Some discussion: The kurtosis enters in the maximum wave height
+        distribution function as a nonlinear correction to the Rayleigh distribution:
+        when the kurtosis tends to 3, the expected Gaussian value, then the dis-
+        tribution function tends to the Rayleigh distribution.
+        That's why it is used the cumulant k40, which represents from 3 (Gaussian).
+        """
+        return (np.pi/np.sqrt(3) * (self.BFI2D()**2)).rename("kurtosis")
 
     def stats(self, stats, fmin=None, fmax=None, dmin=None, dmax=None, names=None):
         """Calculate multiple spectral stats into a Dataset.
